f59b2dec5fba1ac3b5dc7761b578f4de5b9da5d1
xiangyuzhi <zhixy2021@mail.sustech.edu.cn>
2024-06-11 21:56:17 +0800

    concise main

--- read_write.cc
+++ read_write.cc
@@ -29,50 +29,35 @@ void rdma_read(rdma_context *ctx) {
   printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-  printf("This is the concise version of perftest.\n");
-
-  rdma_parameter user_param;
-  int ret_parser = user_param.parser(argv, argc);
-  if (ret_parser) {
-    printf("Failed to parser parameter.\n");
-    exit(0);
-  }
-  //   user_param.verb = READ;
+void rdma_init(
+    rdma_context &ctx, rdma_parameter &user_param, rdma_comm &user_comm,
+    message_context *&my_dest, message_context *&rem_dest) {
   user_param.verb = WRITE;
 
   force_dependecies(&user_param);
-
-  rdma_comm user_comm;
-
-  rdma_context ctx;
-  struct message_context *my_dest = NULL;
-  struct message_context *rem_dest = NULL;
-
   memset(&ctx, 0, sizeof(rdma_context));
-
   memset(&user_comm, 0, sizeof(rdma_comm));
 
   struct ibv_device *ib_dev = ctx_find_dev(&user_param.ib_devname);
   if (!ib_dev) {
     fprintf(stderr, " Unable to find the Infiniband/RoCE device\n");
-    return 0;
+    exit(0);
   }
 
   ctx.context = ctx_open_device(ib_dev, &user_param);
   if (!ctx.context) {
     fprintf(stderr, " Couldn't get context for the device\n");
-    return 0;
+    exit(0);
   }
 
   if (check_link(ctx.context, &user_param)) {
     fprintf(stderr, " Couldn't get context for the device\n");
-    return 0;
+    exit(0);
   }
 
   if (create_comm_struct(&user_comm, &user_param)) {
     fprintf(stderr, " Unable to create RDMA_CM resources\n");
-    return 0;
+    exit(0);
   }
 
   if (user_param.machine == SERVER) {
@@ -83,7 +68,7 @@ int main(int argc, char *argv[]) {
 
   if (establish_connection(&user_comm)) {
     fprintf(stderr, " Unable to init the socket connection\n");
-    return 0;
+    exit(0);
   }
 
   // exchange_versions(&user_comm, &user_param);
@@ -91,25 +76,26 @@ int main(int argc, char *argv[]) {
   if (check_mtu(ctx.context, &user_param, &user_comm)) {
     fprintf(stderr, " Couldn't get context for the device\n");
     dealloc_comm_struct(&user_comm, &user_param);
-    return 0;
+    exit(0);
   }
 
-  MAIN_ALLOC(
-      my_dest, struct message_context, user_param.num_of_qps, free_rdma_params);
+  my_dest = (struct message_context *)malloc(
+      sizeof(struct message_context) * (user_param.num_of_qps));
   memset(my_dest, 0, sizeof(struct message_context) * user_param.num_of_qps);
-  MAIN_ALLOC(
-      rem_dest, struct message_context, user_param.num_of_qps, free_my_dest);
+  rem_dest = (struct message_context *)malloc(
+      sizeof(struct message_context) * (user_param.num_of_qps));
   memset(rem_dest, 0, sizeof(struct message_context) * user_param.num_of_qps);
 
   if (alloc_ctx(&ctx, &user_param)) {
     fprintf(stderr, "Couldn't allocate context\n");
-    return 0;
+    exit(0);
   }
 
   if (ctx_init(&ctx, &user_param)) {
     fprintf(stderr, " Couldn't create IB resources\n");
     dealloc_ctx(&ctx, &user_param);
-    goto free_mem;
+    free(rem_dest);
+    exit(0);
   }
 
   /* Set up the Connection. */
@@ -140,21 +126,37 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // user_param.print_para();
-  // ctx.print_para();
-
   if (user_param.machine == SERVER) {
     if (ctx_close_connection(&user_comm, my_dest, rem_dest)) {
       fprintf(stderr, "Failed to close connection between server and client\n");
-      goto free_mem;
+      free(rem_dest);
+      exit(-1);
     }
 
     free(rem_dest);
     free(my_dest);
     free(user_param.ib_devname);
     free(user_comm.rdma_params);
-    return SUCCESS;
+    exit(-1);
   }
+}
+
+int main(int argc, char *argv[]) {
+  printf("This is the concise version of perftest.\n");
+
+  rdma_parameter user_param;
+  int ret_parser = user_param.parser(argv, argc);
+  if (ret_parser) {
+    printf("Failed to parser parameter.\n");
+    exit(0);
+  }
+
+  rdma_comm user_comm;
+  rdma_context ctx;
+  struct message_context *my_dest = NULL;
+  struct message_context *rem_dest = NULL;
+
+  rdma_init(ctx, user_param, user_comm, my_dest, rem_dest);
 
   // write api
   write_init(&ctx, &user_param, rem_dest);
@@ -164,41 +166,14 @@ int main(int argc, char *argv[]) {
   read_init(&ctx, &user_param, rem_dest);
   rdma_read(&ctx);
 
-  //   printf(RESULT_LINE);
-  //   printf("%s", RESULT_FMT_LAT);
-  //   printf(RESULT_EXT);
-
-  // user_param.print_para();
-  // print_ctx(&ctx);
-
-  //   for (int i = 1; i < 24; ++i) {
-  //     user_param.size = (uint64_t)1 << i;
-  //     if (user_param.verb == READ) {
-  //       if (run_iter_lat(&ctx, &user_param)) {
-  //         goto free_mem;
-  //       }
-  //     } else if (user_param.verb == WRITE) {
-  //       if (run_iter_lat_write(&ctx, &user_param)) {
-  //         goto free_mem;
-  //       }
-  //     }
-  //     print_report_lat(&user_param);
-  //   }
-
   if (ctx_close_connection(&user_comm, my_dest, rem_dest)) {
     fprintf(stderr, "Failed to close connection between server and client\n");
-    goto free_mem;
+    free(rem_dest);
+    return FAILURE;
   }
   free(rem_dest);
   free(my_dest);
   free(user_param.ib_devname);
   free(user_comm.rdma_params);
   return SUCCESS;
-
-free_rdma_params:
-  free(user_comm.rdma_params);
-free_my_dest:
-  free(my_dest);
-free_mem:
-  free(rem_dest);
 }
